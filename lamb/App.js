ecff0345084ecc39ed73fa969a9f39bc556b94ec
GUILHERME BARBOSA <gui.diascampos@outlook.com>
2020-01-08 15:49:41 -0300

    Components publicações

--- lamb/App.js
+++ lamb/App.js
@@ -1,12 +1,24 @@
 
 import React, { Component } from 'react';
 import Header from './src/componentes/Header'
-
+import {View, Text} from 'react-native'
+import Post from './src/componentes/Post'
 
 export default class App extends Component{
 	render(){
+		const comments = [{
+			nickname: 'Joana Elena',
+			comment: 'Excelente foto'
+		}, {
+			nickname: 'Guilherme Dias',
+			comment: 'Cara pica'
+		}]
+
 		return(
-			<Header />
+			<View style={{flex: 1}}>
+				<Header />
+				<Post image={require('./assets/imgs/fence.jpg')} comments={comments}/>
+			</View>
 		)
   	}
 }
